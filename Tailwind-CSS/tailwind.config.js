31df4bcc006edbf293f0b36f1c437df4fa14f4fd
abhishake21 <y.abhishake210@gmail.com>
2022-02-27 00:10:06 +0530

    exaple portfolio using tailwind

---
+++ Tailwind-CSS/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "public/*html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#17171f',
+        'selected-text': '#a3a3ff',
+        'theme': '#3f3fff',
+        'nav': '#404053',
+        'secondary': '#9191a4',
+        'badge': '#3f3f51',
+        'input-border': '#565666',
+        'input': '#2a2a35'
+      },
+      fontFamily: {
+        'poppins': ['Poppins','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
